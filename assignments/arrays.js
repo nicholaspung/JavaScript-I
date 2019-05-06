18f372627f116f805a0a14d189a7f01a84ef8a4b
Nicholas Pung <nicholaspung>
2019-05-06 16:55:50 -0700

    changed arrays last exercise to encompass entire car object

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -142,7 +142,7 @@ let BMWAndAudi = [];
 
 for (let i=0; i < inventory.length; i++) {
     if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
-        BMWAndAudi.push(inventory[i].car_model);
+        BMWAndAudi.push(inventory[i]);
     }
 }
 
